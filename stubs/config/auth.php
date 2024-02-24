b8543b863a597a0b0437d09d922d6d061b35690f
= <inman.turbo@gmail.com>
2024-02-24 17:19:57 -0500

    update readme

--- stubs/config/auth.php
+++ stubs/config/auth.php
@@ -40,7 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-
+        
         'api' => [
             'driver' => 'passport',
             'provider' => 'users',
