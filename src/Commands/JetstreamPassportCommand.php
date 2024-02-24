f209ef08065e180f8ad241c021e6ef9d9d64e053
= <inman.turbo@gmail.com>
2024-02-24 16:28:41 -0500

    update install command to use stubs

--- src/Commands/JetstreamPassportCommand.php
+++ src/Commands/JetstreamPassportCommand.php
@@ -3,6 +3,7 @@
 namespace HeaderX\JetstreamPassport\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class JetstreamPassportCommand extends Command
 {
@@ -16,5 +17,42 @@ class JetstreamPassportCommand extends Command
         $this->call('passport:keys');
         $this->call('passport:install', ['--uuids']);
         $this->call('passport:client', ['--personal' => true, '--name' => 'Laravel Personal Access Client']);
+
+        $this->replaceInFile('Laravel\\Sanctum\\HasApiTokens' , 'Laravel\\Passport\\HasApiTokens', app_path('Models/User.php'));
+
+        $this->copyFiles();
+    }
+
+    /**
+     * Replace a given string within a given file.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $path
+     * @return void
+     */
+    protected function replaceInFile($search, $replace, $path)
+    {
+        file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
+    }
+
+    protected function copyFiles()
+    {
+        $this->info('Copying files...');
+
+        $sourceDir = realpath(__DIR__.'/../../stubs/');
+        $destinationDir = base_path();
+
+        $files = File::allFiles($sourceDir);
+
+        foreach ($files as $file) {
+            $destinationFilePath = $destinationDir.'/'.$file->getRelativePathname();
+            File::ensureDirectoryExists(dirname($destinationFilePath));
+            File::copy($sourceFile = $file->getPathname(), $destinationFilePath);
+            // check verbosity
+            if ($this->output->isVerbose()) {
+                $this->line('<info>Copied</info> '.$sourceFile.' <info>to</info> '.$destinationFilePath);
+            }
+        }
     }
 }
